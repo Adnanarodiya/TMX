7e414cbb1e8278b705da6396c4d90c8ea080c2ea
Adnan Arodiya <aiarodiya07@gmail.com>
2024-06-06 13:28:09 +0530

    Affiliate Program page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import ServiceCard from "./components/ServiceCard";
 import GloabCard from "./components/GloabCard";
 import BenefitsCard from "./components/BenefitsCard";
+import AffiliateCard from "./components/AffiliateCard";
 import Footer from "./components/Footer";
 import ClientSayCar from "./components/ClientSayCar";
 
@@ -267,6 +268,61 @@ function App() {
           >
             Become a member
           </button>
+        </div>
+      </section>
+
+      <section className=" relative py-16 mx-3  mt-[50px] sm:mt-[100px]">
+        <div className="absolute inset-0">
+          <div className="w-full h-full bg-cover bg-fixed bg-[url('/Affiliate-Program.png')]">
+            <div className="absolute inset-0 bg-[#58427C] opacity-80"></div>
+          </div>
+        </div>
+        <div className="relative z-0 container mx-auto px-4">
+          <h1 className="text-[35px] sm:text-[42px] lg:text-[50px] font-semibold text-center text-white">
+            Affiliate Program
+          </h1>
+          <p className="mt-3 text-center mx-auto text-white">
+            Join our affiliate program and get paid
+          </p>
+
+          <div className="grid grid-cols-1 mx-6 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
+            <AffiliateCard
+              imgSrc="/commissions.svg"
+              title="Earn Commissions"
+              description="Get commissions for every client you bring."
+            />
+            <AffiliateCard
+              imgSrc="/network.svg"
+              title="Expand Network"
+              description="Connect with a larger network of potential clients and partners."
+            />
+            <AffiliateCard
+              imgSrc="partnerships.svg"
+              title="Exclusive Partnerships"
+              description="Benefit from exclusive partnership opportunities."
+            />
+            <AffiliateCard
+              imgSrc="/opportunities.svg"
+              title="Growth Opportunities"
+              description="Access tools and resources to help your business grow."
+            />
+          </div>
+
+          <div className="mt-20">
+            <h4 className="text-white text-center text-2xl md:text-4xl mb-4 ">
+              Get paid
+              <span className="text-red-200"> anytime </span>
+              anywhere
+            </h4>
+            <div className="flex justify-center">
+              <button
+                type="button"
+                className="bg-orange text-white h-[45px] px-[18px] rounded-full  hover:bg-red-700 transition"
+              >
+                Join Now
+              </button>
+            </div>
+          </div>
         </div>
       </section>
 
---
+++ src/components/AffiliateCard.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+export default function AffiliateCard({ imgSrc, title, description }) {
+    return (
+        <div className="bg-white p-4">
+            <div>
+                <img className="mx-auto" src={imgSrc} alt="" />
+            </div>
+            <h4 className="mt-[10px] text-[20px] sm:text-[25px] font-medium text-black text-center ">
+                {title}
+            </h4>
+            <p className="pr-1 mt-[10px] text-center text-base text-gray-600">
+                {description}
+            </p>
+        </div>
+    )
+}
